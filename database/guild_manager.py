567bdf304bc7c27d48c5376b55c759077699fa0b
Andi Amjad <andiamjad@gmail.com>
2026-06-27 02:45:26 +0800

    add database guild join

---
+++ database/guild_manager.py
@@ -0,0 +1,17 @@
+from database.database import get_connection
+
+def add_guild(guild_id: int, nama_guild: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO guild_db (guild_id, nama_guild)
+        VALUES (%s, %s)
+        ON DUPLICATE KEY UPDATE
+            nama_guild = VALUES(nama_guild)
+    """, (guild_id, nama_guild))
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
--- events/guild_join.py
+++ events/guild_join.py
@@ -1,4 +1,10 @@
 import discord
 
+from utils.logger import send_log
+from database.guild_manager import add_guild
+
+
 async def handle_guild_join(guild: discord.Guild):
+    add_guild(guild.id, guild.name)
+
     print(f"Bot bergabung ke server: {guild.name} ({guild.id})")
